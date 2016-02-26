af67c14bd02ba1206912a3a27452c41eadaf51a5
Isaiah Weaver <iswea1@morgan.edu>
2016-02-26 04:18:26 -0500

    added new images and changed nav contoller color

--- Flicks/AppDelegate.swift
+++ Flicks/AppDelegate.swift
@@ -9,36 +9,54 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate{
 
     var window: UIWindow?
+    
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
         let nowPlayingNavigationController = storyboard.instantiateViewControllerWithIdentifier("MoviesNavigationController") as! UINavigationController
         let nowPlayingViewController = nowPlayingNavigationController.topViewController as! MoviesViewController
         nowPlayingViewController.endpoint = "now_playing"
         nowPlayingNavigationController.tabBarItem.title = "Now Playing"
-        nowPlayingNavigationController.tabBarItem.image = UIImage(named: "nowplaying")
+        nowPlayingNavigationController.tabBarItem.image = UIImage(named: "film")
         
         
         let topRatedNavigationController = storyboard.instantiateViewControllerWithIdentifier("MoviesNavigationController") as! UINavigationController
+        //Second viewController Setup
         let topRatedViewController = topRatedNavigationController.topViewController as! MoviesViewController
         topRatedViewController.endpoint = "top_rated"
         topRatedNavigationController.tabBarItem.title = "Top Rated"
-        topRatedNavigationController.tabBarItem.image = UIImage(named:"like")
+        topRatedNavigationController.tabBarItem.image = UIImage(named:"Star")
+       
+        //Change nowPlayingNavigation Bar to Black Color, tint and titleTextAttributes to orange
+        nowPlayingNavigationController.navigationBar.barTintColor = UIColor.blackColor()
+        nowPlayingNavigationController.navigationBar.tintColor = UIColor.orangeColor()
+        nowPlayingNavigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orangeColor()]
+        
+        
+        //Change topRated Navigation Bar to black, tint and titleTextAttributes to orange
+        topRatedNavigationController.navigationBar.barTintColor = UIColor.blackColor()
+        topRatedNavigationController.navigationBar.tintColor = UIColor.orangeColor()
+        topRatedNavigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orangeColor()]
         
         
         
+        
+        //Set up of Tab Bar
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [nowPlayingNavigationController,topRatedNavigationController]
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
+        
+        
+        
+        
         
         
         
